6e62274c617b6f392bfd0c0ae2d0ddaa7cc3d1f6
W <as5818956@gmail.com>
2020-12-23 04:50:24 +0900

    add DLNK script  shader

---
+++ Assets/_DLNK/_DLNK Source/_DLNK Libraries/[dlnk Script Library]/TopDown Wizard/Source/Scripts/Inspector/TDzonecustomInspector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TDZone))]
+public class TDzonecustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        TDZone tdZone = (TDZone)target;
+        tdZone.florname = GUILayout.TextField(tdZone.florname);
+        if (GUILayout.Button("Create New Floor"))
+        {
+            tdZone.OnDo();
+        }
+        if (GUILayout.Button("Create New Trigger"))
+        {
+            tdZone.OnTrigCreate();
+        }
+        if (GUILayout.Button("Change Ceiling Shadow Materials"))
+        {
+            tdZone.OnCeilingTint();
+        }
+        DrawDefaultInspector();
+    }
+}
