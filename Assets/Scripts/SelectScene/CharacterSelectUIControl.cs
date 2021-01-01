6d29d475dc7fcc937c59e9eafe39e95dde91ee16
Wamg <as5818956@gmail.com>
2021-01-01 15:35:47 +0900

    Add Icon

---
+++ Assets/Scripts/SelectScene/CharacterSelectUIControl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CharacterSelectUIControl : MonoBehaviour
+{
+    [SerializeField] Button P1Submit;
+    [SerializeField] Button P1Previous;
+    [SerializeField] Button P1Next;
+    [SerializeField] Button P2Submit;
+    [SerializeField] Button P2Previous;
+    [SerializeField] Button P2Next;
+
+    private StartGame startGame;
+    private bool P1Submit_IsPressed;
+    private bool P2Submit_IsPressed;
+    private void Awake()
+    {
+        startGame = GetComponent<StartGame>();
+    }
+
+    private void Update()
+    {
+        StartGame();
+    }
+    public void OnP1SubmitPressed()
+    {
+        P1Previous.gameObject.SetActive(false);
+        P1Next.gameObject.SetActive(false);
+        P1Submit_IsPressed = true;
+    }
+
+    public void OnP2SubmitPressed()
+    {
+        P2Previous.gameObject.SetActive(false);
+        P2Next.gameObject.SetActive(false);
+        P2Submit_IsPressed = true;
+    }
+
+    public void StartGame()
+    {
+        switch (GameManager.Instance.gameMode)
+        {
+            case GameMode.OnePlayer:
+                if (P1Submit_IsPressed)
+                {
+                    startGame.OnPlayButtonPress();
+                }
+                break;
+            case GameMode.TwoPlayer:
+                if (P1Submit_IsPressed && P2Submit_IsPressed)
+                {
+                    startGame.OnPlayButtonPress();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/SelectScene/SelectScenePlayerManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System;
+using UnityEngine.UI;
+
+public class SelectScenePlayerManager : MonoBehaviour
+{
+    [SerializeField] GameObject _p2Selection;
+    [SerializeField] Camera _p1Camera;
+    [SerializeField] Camera _p2Camera;
+    [SerializeField] CinemachineFreeLook _p1FollowCamera;
+    [SerializeField] CinemachineFreeLook _p2FollowCamera;
+    [SerializeField] Canvas _p2UI;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        switch (GameManager.Instance.gameMode)
+        {
+            case GameMode.OnePlayer:
+                SetOnePlayerMode();
+                break;
+            case GameMode.TwoPlayer:
+                SetTwoPlayerMode();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SetOnePlayerMode()
+    {
+        _p2Selection.SetActive(false);
+        _p2UI.gameObject.SetActive(false);
+        _p2Camera.gameObject.SetActive(false);
+        _p2FollowCamera.gameObject.SetActive(false);
+        _p1Camera.rect = new Rect(0, 0, 1.0f, 1.0f);
+    }
+
+    private void SetTwoPlayerMode()
+    {
+        _p2Selection.SetActive(true);
+        _p2UI.gameObject.SetActive(true);
+        _p2Camera.gameObject.SetActive(true);
+        _p2FollowCamera.gameObject.SetActive(true);
+        _p1Camera.rect = new Rect(0, 0.5f, 1.0f, 0.5f);
+        _p2Camera.rect = new Rect(0, 0, 1.0f, 0.5f);
+
+    }
+
+    public void SetPlayerContextActive(int playerindex)
+    {
+
+    }
+}
