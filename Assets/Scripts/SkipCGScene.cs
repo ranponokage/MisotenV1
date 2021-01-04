1fc22983589ad5590393c8240d4d9a4f8952bb9d
Wamg <as5818956@gmail.com>
2021-01-05 04:35:12 +0900

    CGScene sort of done

---
+++ Assets/Scripts/SkipCGScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using Michsky.LSS;
+
+public class SkipCGScene : MonoBehaviour
+{
+    Player _player;
+    [SerializeField] LoadingScreenManager _lsm;
+
+    private void Awake()
+    {
+        _player = ReInput.players.GetPlayer(0);
+    }
+
+    private void Update()
+    {
+        if(_player.GetAnyButton())
+        {
+            _lsm.LoadScene("PlayerSelect");
+        }
+
+    }
+}
---
+++ Assets/Scripts/TimeLine/LoadOnActivation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Michsky.LSS;
+
+public class LoadOnActivation : MonoBehaviour
+{
+    [SerializeField] LoadingScreenManager _lsm;
+
+
+    void OnEnable()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+
+        _lsm.LoadScene("PlayerSelect");
+
+    }
+}
